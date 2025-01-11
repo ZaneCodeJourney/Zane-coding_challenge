ab26f97921a2f0633fb99d591f58207ef4faf4bd
Ziyang Wen <wenziyang007@gmail.com>
2025-01-11 15:48:23 +0800

    Create mstest project

--- obj/Debug/net8.0/LibraryManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58b97d898dfa7228a33ab6c22fc27f849667413")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a020d48d6ee19bdd39764d19edbc7b518e3af53")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
